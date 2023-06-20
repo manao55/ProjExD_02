67816ccb3d5ddf656f7eb7ea75d8c1d2711bd8ff
manaty <c0a2200400@edu.teu.ac.jp>
2023-06-20 14:27:37 +0900

    練習１

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -1,3 +1,4 @@
+import random
 import sys
 import pygame as pg
 
@@ -13,6 +14,13 @@ def main():
     kk_img = pg.transform.rotozoom(kk_img, 0, 2.0)
     clock = pg.time.Clock()
     tmr = 0
+    bd_img = pg.Surface((20, 20))  # 練習１
+    pg.draw.circle(bd_img, (255, 0, 0), (10, 10), 10)
+    bd_img.set_colorkey((0, 0, 0))
+    x = random.randint(0, WIDTH)
+    y = random.randint(0, HEIGHT)
+    bd_rct = bd_img.get_rect()
+    bd_rct.center = x, y
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: 
