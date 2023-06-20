63c0fb8aa329ed446700112bc7a5ebc07e9e42b5
manaty <c0a2200400@edu.teu.ac.jp>
2023-06-20 14:41:39 +0900

    練習３

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -4,7 +4,12 @@ import pygame as pg
 
 
 WIDTH, HEIGHT = 1600, 900
-
+delta = {
+    pg.K_UP: (0, -5),
+    pg.K_DOWN: (0, +5),
+    pg.K_LEFT: (-5, 0),
+    pg.K_RIGHT: (+5, 0),
+}
 
 def main():
     pg.display.set_caption("逃げろ！こうかとん")
@@ -12,12 +17,15 @@ def main():
     bg_img = pg.image.load("ex02/fig/pg_bg.jpg")
     kk_img = pg.image.load("ex02/fig/3.png")
     kk_img = pg.transform.rotozoom(kk_img, 0, 2.0)
+    # こうかとんSurface（kk_img）からこうかとんRect（kk_rct）を抽出する
+    kk_rct = kk_img.get_rect()
+    kk_rct.center = 900, 400
     bd_img = pg.Surface((20, 20))  # 練習１
+    bd_img.set_colorkey((0, 0, 0))  # 黒い部分を透明にする
     pg.draw.circle(bd_img, (255, 0, 0), (10, 10), 10)
-    bd_img.set_colorkey((0, 0, 0))
     x = random.randint(0, WIDTH)
     y = random.randint(0, HEIGHT)
-     # 爆弾Surface（bd_img）から爆弾Rect（bd_rct）を抽出する
+    # 爆弾Surface（bd_img）から爆弾Rect（bd_rct）を抽出する
     bd_rct = bd_img.get_rect()
     # 爆弾Rectの中心座標を乱数で指定する
     bd_rct.center = x, y 
@@ -29,14 +37,21 @@ def main():
         for event in pg.event.get():
             if event.type == pg.QUIT: 
                 return
-
+        key_lst = pg.key.get_pressed()
+        sum_mv = [0, 0]  # 合計移動量
+        for k, mv in delta.items():
+            if key_lst[k]: 
+                sum_mv[0] += mv[0]
+                sum_mv[1] += mv[1]
+        kk_rct.move_ip(sum_mv)
+ 
         screen.blit(bg_img, [0, 0])
-        screen.blit(kk_img, [900, 400])
+        screen.blit(kk_img, kk_rct)
         bd_rct.move_ip(vx, vy)  # 練習２
         screen.blit(bd_img, bd_rct)
         pg.display.update()
         tmr += 1
-        clock.tick(10)
+        clock.tick(50)
 
 
 if __name__ == "__main__":
